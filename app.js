cd8143ec9844eece94b500291cf5f6bc58b5ef0f
Bharti kumari <bhartikumari20@navgurukul.org>
2021-07-12 17:33:31 +0530

    updated and deleted data

--- app.js
+++ app.js
@@ -1,40 +1,77 @@
 const express = require('express')
 const app = express()
-const jsonData = require('./index.json')
+const allData = require('./index.json')
 const fs = require('fs')
-const { stringify } = require('querystring')
-
 
 app.use(express.json())
 
 app.get('/all_data',(req, res) =>{
-    res.send(jsonData);
+    res.send(allData);
 });
 
 app.get('/:id', (req,res) => {
     var id = req.params.id
     let i = 0
-    while(i<jsonData.length) {
-        if(id == jsonData[i].id) {
-            res.send(jsonData[i])
+    while(i<allData.length) {
+        if(id == allData[i].id) {
+            res.send(allData[i])
         }i++
     };
 });
 
 app.post('/createData', (req,res)=>{
     var data = {
-        id: jsonData.length + 1,
+        id: allData.length + 1,
         name : req.body.name,
         class:req.body.class,
         city:req.body.city
     }
     res.send(data)
     console.log(data)
-    jsonData.push(data)
-    var stringData = JSON.stringify(jsonData, null, 2)
+    allData.push(data)
+    var stringData = JSON.stringify(allData, null, 2)
     fs.writeFileSync('index.json',stringData)
 });
+
+app.put('/updating/:id', (req,res) =>{
+    var i = 0
+    var myId = req.params.id
+    while(i<allData.length) {
+        if(allData[i]['id'] == myId){
+                allData[i]['name'] = req.body.name,
+                allData[i]['class'] = req.body.class,
+                allData[i]['city'] = req.body.city
+        }
+        i++
+    };
+    var string = JSON.stringify(allData, null, 2)
+    fs.writeFileSync('index.json',string)
+    res.send(allData)
+    console.log(string)
+});
+
+app.delete('/deleting/:id', (req,res) =>{
+    var i = 0;
+    var uniqueId = req.params.id
+    while(i<allData.length) {
+        if(allData[i]['id'] == uniqueId) {
+            allData.splice(i, 1)
+            break;
+        }i++
+    };
+    var string = JSON.stringify(allData, null, 2)
+    fs.writeFileSync('index.json',string)
+    res.send(allData)
+    console.log(string)
+});
+
+
 app.listen(4000, () =>{
     console.log('Running on server---')
 });
 
+   
+// var arr = [1, 2, 1, 4,];
+// let i = 0
+// var removed = arr.splice(5,4);
+// console.log(arr)
